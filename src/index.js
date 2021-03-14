4db04b1d91df05db4ceb508e1c0f5b4bfcbbdb9a
Angry Fennec <shellfix@gmail.com>
2021-03-14 16:33:21 +0300

    feat: add partially correct check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+      let result = true;
+      let stack = new Stack();
+  
+      for (let i = 0; i < str.length; i++) {
+          const c = str[i];
+          let isStart = false;
+          bracketsConfig.forEach(item => {
+            if (c === item[0]) {
+              isStart = true;
+            }
+          });
+          if (isStart) {
+            stack.push(c);
+            continue;
+          }
+
+          if (stack.size() === 0) {
+              return false;
+          }
+  
+          const p = stack.pop();
+          let isPair = false;
+          bracketsConfig.forEach(item => {
+            if (p === item[0] && c === item[1]) {
+              isPair = true;
+            }
+          });
+          if (isPair) {
+            continue;
+          } else {
+            result = false;
+            return result;
+          }
+      }
+      if (stack.size() > 0) {
+          return false;
+      } 
+      return result;
+}
+
+
+class Stack {  
+  constructor() {  
+    this.stack = [];    
+  }
+  
+  push(item) {
+    this.stack.push(item);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  size() {
+    return this.stack.length;
+  }
 }
